91f85757199fb20f0e038c0572836f9f002f6b45
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-07-07 20:17:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,6 +31,6 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Linkbot:Linkbot@cluste
 DATABASE_NAME = environ.get('DATABASE_NAME', "Linkbot")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
+SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'onylinks.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'e0b993ab824859c3375b05a636c32f5b61d528d2')
